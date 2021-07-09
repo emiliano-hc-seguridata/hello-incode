445d8e97d571145f6b977523d11213fb056b541e
sofseguridata <slozada@seguridata.com>
2021-07-09 10:04:18 -0500

    9julio

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-const apiURL = "https://demo-api.incodesmile.com/";
+
+var apiURL = "https://demo-api.incodesmile.com/";
 const apiKey = "570c70d1693636fdc200713415ebc3973afbdf19";
-
 
 const helloSdk = window.Hello.create({
   apiKey: apiKey,
   apiURL: apiURL,
 });
 
-helloSdk.renderLogin(document.getElementById('root'), {
+class App extends React.Component {
 
-  onSuccess: r => {
-    console.log('onSuccess', r)
-    document.getElementById('root').innerHTML = `Welcome Back, your token is ${r.token}`
-  },
-  onError: r => {
-    console.log('on error', r)
-  },
-})
+  constructor(props) {
+    super(props);
+  }
 
-function App() {
-  return (
-    <div className="App">
+  renderLogin() {
+
+    
+    helloSdk.createSession('MX').then(token => {
+      console.log(token)
+      navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(function(mediaStream) {
+        helloSdk.renderLogin(document.getElementById('incode'), {
+
+          onSuccess: r => {
+            console.log('onSuccess', r)
+            document.getElementById('root').innerHTML = `Welcome Back, your token is ${r.token}`
+          },
+          onError: r => {
+            console.log('on error', r)
+            //no estas en sistema te vamos a registrar
+          },
+        },token)
+      }).catch(function(err) {
+        console.log(err)
+      });
+    })
+}
+
+  render (){
+    return <div id="incode" className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p id='success'>
@@ -37,9 +55,10 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={this.renderLogin}>load login</button>
       </header>
     </div>
-  );
+  }
 }
 
 export default App;
